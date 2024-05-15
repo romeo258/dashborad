ada6d3fe11eff0182fa0334e62a961a923ec6d38
Romeo <temgouaromeo@gmail.com>
2024-05-15 12:21:09 +0100

    correction bug jquery

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,29 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit{
-
-  constructor() { }
+export class HomeComponent implements OnInit {
+  showScrollButton: boolean = false;
 
   ngOnInit(): void {
-    this.scrollToTop();
+    // Votre code initial ici
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollDistance = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    this.showScrollButton = scrollDistance > 100;
   }
 
   scrollToTop() {
-    const scrollButton = document.querySelector('.scroll-to-top') as HTMLElement;
-    
-    if (scrollButton) {
-        scrollButton.addEventListener('click', () => {
-            window.scrollTo({
-                top: 0,
-                behavior: 'smooth'
-            });
-        });
-    }
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
   }
-
 }
